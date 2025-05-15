6e107b10f2ac8ad9092aab5c07057f11436960d2
Shivanshu <cs23b058@smail.iitm.ac.in>
2025-05-15 09:53:47 +0530

    add directory structure

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<fstream>
+#include<vector>
+#include<string>
+
+void simulate(std::string directory){
+    int ICache[256], DCache[256];
+    // taking input 
+    std::ifstream dcache("./input/"+directory + "/DCache.txt");
+    std::ifstream icache("./input/"+directory + "/ICache.txt");
+}
+int main(){
+
+}
